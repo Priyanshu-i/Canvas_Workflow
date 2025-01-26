946c22b3be67ee2e18537740aa986dc657d39a3e
Priyanshu Singh <priyanshu40507@gmail.com>
2025-01-26 22:30:35 +0530

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import WorkflowCanvas from "./components/WorkflowCanvas"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <WorkflowCanvas />
+    </main>
+  )
+}
+
